c988ffa1db184fec3bb92d9e8b06fbd5bdb2407b
Alon Gamliel <alon.gamliel@fluxfederation.com>
2018-04-12 22:29:05 +1200

    cleanup

--- src/Defeat.js
+++ src/Defeat.js
@@ -2,6 +2,6 @@
 
 import React from 'react';
 
-const Defeat = <h1>Defeat!</h1>;
+const Defeat = () => <h1>Defeat!</h1>;
 
 export default Defeat;
--- src/Victory.js
+++ src/Victory.js
@@ -2,6 +2,6 @@
 
 import React from 'react';
 
-const Victory = <h1>Victory!</h1>;
+const Victory = () => <h1>Victory!</h1>;
 
 export default Victory;
